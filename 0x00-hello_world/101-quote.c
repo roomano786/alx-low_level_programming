d8ddec4ea1d52d6f605d79270f4b4fc1ede13981
roomano786 <radelaisse@gmail.com>
2022-12-11 07:15:01 -0800

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints to the standard error
+ *
+ * Return: Always 1 (Error)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
