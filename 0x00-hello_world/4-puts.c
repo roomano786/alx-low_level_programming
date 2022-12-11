f05ff18a613c1317fb579b902e561f5489e7557f
roomano786 <radelaisse@gmail.com>
2022-12-11 04:43:48 -0800

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: program that prints using the puts() method
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzle\"");
+	return (0);
+}
