1234365d87866c3c612025121f22bcfad13a7bc9
roomano786 <radelaisse@gmail.com>
2022-12-11 04:54:45 -0800

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints using the printf() method
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
