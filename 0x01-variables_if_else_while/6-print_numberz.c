bc83a58b35a64c83b93dabd240d33f4193773661
roomano786 <radelaisse@gmail.com>
2022-12-12 09:42:22 -0800

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: print numbers using putch from 0 to 9
+  *
+  * Return: Always 0 (Success)
+  *
+  */
+
+int main(void)
+{
+	int number;
+
+	while (number < 10)
+	{
+		putchar(number + '0');
+
+		number++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
