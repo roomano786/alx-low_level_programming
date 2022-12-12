8a2cc80275528ec6033a300014aeaef3d43cec90
roomano786 <radelaisse@gmail.com>
2022-12-12 09:21:33 -0800

    aa

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: program that prints all single digit of base 10 staring from 0
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int start = 0;
+
+	while (start < 10)
+	{
+		printf("%d",start);
+		start++;
+	}
+
+	putchar('\n');
+	return (0);
+}
