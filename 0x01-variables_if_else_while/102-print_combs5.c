27c8ccdc466aaae3c670bc9ae1b73fb0f86ee628
roomano786 <radelaisse@gmail.com>
2022-12-29 05:12:39 -0800

    a

---
+++ 0x01-variables_if_else_while/102-print_combs5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Desctiption: prints all possible combinations of two-digits numbers
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 100; num1++)
+	{
+		for (num2 = 0; num2 < 100; num2++)
+		{
+			putchar((num1 / 10) + 48);
+			putchar((num1 % 10) + 48);
+			putchar(' ') ;
+			putchar((num2 / 10) + 48);
+			putchar((num2 % 10) + 48);
+
+			if (num1 != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
