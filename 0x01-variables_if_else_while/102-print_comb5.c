ab53cfffbbb245d569746d57fae8791350c82efb
roomano786 <radelaisse@gmail.com>
2022-12-29 05:20:37 -0800

    a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,24 +10,30 @@
 
 int main(void)
 {
-	int num1, num2;
+	int num1 = 0, num2;
 
-	for (num1 = 0; num1 < 100; num1++)
+	while (num1 <= 99)
 	{
-		for (num2 = 0; num2 < 100; num2++)
+		num2 = 0;
+		while (num2 <= 99)
 		{
-			putchar((num1 / 10) + 48);
-			putchar((num1 % 10) + 48);
-			putchar(' ') ;
-			putchar((num2 / 10) + 48);
-			putchar((num2 % 10) + 48);
-
-			if (num1 != 98 || num2 != 99)
+			if (num2 != num1)
 			{
-				putchar(',');
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
 				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					purchar(' ');
+				}
 			}
+			num2++;
 		}
+		num1++;
 	}
 	putchar('\n');
 	return (0);
