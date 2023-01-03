a03b901d374ac5da0e625bc39a37da83d9422c44
roomano786 <radelaisse@gmail.com>
2023-01-03 12:07:43 -0800

    a

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _isalpha - checks for alphabet char
+  *
+  * @c: int parameter
+  *
+  * Return: 1 if c is letter 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
+		return (1);
+	return (0);
+}
