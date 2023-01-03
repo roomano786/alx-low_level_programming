2ef2ca77217896ed383f04cdf140a06547392ff6
roomano786 <radelaisse@gmail.com>
2023-01-03 11:41:44 -0800

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 {
 	int times, letter;
 
-	for (times = 0; times < 9; times++)
+	for (times = 0; times <= 9; times++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
