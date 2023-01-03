a39409990d5662ba0c463c286d1c02f9f5795c99
roomano786 <radelaisse@gmail.com>
2023-01-03 11:40:20 -0800

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * Description: func that prints the alphabet in 10 times
+  * Return
+ */
+
+void print_alphabet_x10(void)
+{
+	int times, letter;
+
+	for (times = 0; times < 9; times++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
