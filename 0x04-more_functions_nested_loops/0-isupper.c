ea3295dc44ba0a1b7e7ac5961cfe74b9980ecd51
roomano786 <radelaisse@gmail.com>
2023-01-03 16:05:00 -0800

    a

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+  * _isupper - checks for uppercase char
+  *
+  * @c: int func param
+  *
+  * Return: 1 if c is upper, 0 otherwise
+  */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
